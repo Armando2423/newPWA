e8281d2910e2c78074760f7e5cbd6107016a0b2f
Armando2423 <sergio.reyes.21m@utzmg.edu.mx>
2025-03-25 20:22:54 -0600

    add entorn variable (again)

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3005, // Usa el puerto de Render o el 3005 localmente
-    host: true // Permite acceder desde cualquier IP
+    port:  3005, // Usa el puerto de Render o el 3005 localmente
+    host: true, // Permite acceder desde cualquier IP
+    headers: {
+      "Content-Security-Policy": "default-src 'self'; img-src * data:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';"
+    }
   },
   preview: {
     port: process.env.PORT || 3000,
     host: true
   }
 })
- 
+  
